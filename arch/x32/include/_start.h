b406cf836087acd1cf9578b4d5d3eb9b5b726a9a
James Larrowe <larrowe.semaj11@gmail.com>
2019-03-24 00:24:20 -0400

    Add gcc wrapper, start stub for sys/socket.h, use more efficient x32 asm

--- arch/x32/include/_start.h
+++ arch/x32/include/_start.h
@@ -3,7 +3,7 @@ __asm__(
 ".globl _start \n"
 "_start: \n"
 "	xor %ebp, %ebp \n"
-"	mov %rsp, %rdi \n"
+"	mov %esp, %edi \n"
 "	and $-0x0f, %esp \n"
 "	call _c_start \n"
 );
---
+++ include/sys/socket.h
@@ -0,0 +1,8 @@
+#ifndef _SYS_SOCKET_H
+#define _SYS_SOCKET_H 1
+
+#include <stddef.h>
+
+typedef size_t socklen_t;
+
+#endif
